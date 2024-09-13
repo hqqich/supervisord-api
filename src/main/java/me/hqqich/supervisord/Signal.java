b478f4ef6fc4d5df4066925e4bf3e008290f9cb4
hqqich <hqqich1314@outlook.com>
2024-09-13 10:52:40 +0800

    初始化项目

---
+++ src/main/java/me/hqqich/supervisord/Signal.java
@@ -0,0 +1,5 @@
+package me.hqqich.supervisord;
+
+public enum Signal {
+    SIGTERM, SIGINT, SIGQUIT, SIGHUP, SIGUSR2, HUP
+}
---
+++ src/main/java/me/hqqich/supervisord/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @author hqqich1314(hqqich1314@outlook.com) .
+ */
+package me.hqqich.supervisord;
